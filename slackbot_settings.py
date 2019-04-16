9ec4bb6ed99fc87defa58da612e67382973305f6
吉川　貴子 <uu036289@CA0933.local>
2019-04-16 17:30:18 +0900

    テスト実装

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,10 +1,13 @@
 # coding: utf-8
 import boto3
+import os
 
 # botアカウントのトークンを指定
-ssm = boto3.client('ssm', region_name='ap-northeast-1')
-parameter = ssm.get_parameter(Name='slackapp_bot_token', WithDecryption=True)
-API_TOKEN = parameter['Parameter']['Value']
+API_TOKEN = os.environ.get('SLACKAPP_BOT_TOKEN')
+if API_TOKEN is None:
+    ssm = boto3.client('ssm', region_name='ap-northeast-1')
+    parameter = ssm.get_parameter(Name='slackapp_bot_token', WithDecryption=True)
+    API_TOKEN = parameter['Parameter']['Value']
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "Sorry but I didn't understand you"
