57df390b94feeaecf87dedf6f79db484bb3bf7e4
吉川　貴子 <uu036289@CA0933.local>
2019-06-19 13:24:12 +0900

    指示に従いボットが発言する

--- helloworld/tests.py
+++ helloworld/tests.py
@@ -4,6 +4,8 @@ from unittest import mock
 import slackbot.dispatcher
 import plugins.hello
 from godd_morning import GoodMornig
+import slack
+import slackbot_settings
 
 class HelloWorldTestCase(TestCase):
 
@@ -42,3 +44,64 @@ class HelloWorldTestCase(TestCase):
         gm.postMessage(channels)
 
         client.chat_postMessage.assert_called_with(channel="CHXS0FH5M",text="おはようございます")
+
+    def test_post_return_no_user(self):
+        data = {}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'no user')
+
+    def test_post_return_unauthorized_user(self):
+        data = {"user_id": "123"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'unauthorized user')
+
+    def test_post_return_no_parameter(self):
+        data = {"user_id": "UB9AVTDT3"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'no parameter')
+
+    def test_post_return_invalid_parameter(self):
+        data = {"user_id": "UB9AVTDT3", "text": "prj-slackapp-test"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'invalid parameter')
+
+    def test_post_return_no_message(self):
+        data = {"user_id": "UB9AVTDT3", "text": "prj-slackapp-test,"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'no message')
+
+    def test_post_return_invalid_channel(self):
+        data = {"user_id": "UB9AVTDT3", "text": "abc,test"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'invalid channel')
+
+    def test_post_call_chat_postMessage(self):
+        client = slack.WebClient(token=slackbot_settings.API_TOKEN)
+        slack.WebClient = mock.MagicMock(return_value=client)
+        client.chat_postMessage = mock.MagicMock()
+
+        data = {"user_id": "UB9AVTDT3", "text": "prj-slackapp-test,test"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test')
+        client.chat_postMessage.assert_called_with(channel="CHXS0FH5M",text="test")
